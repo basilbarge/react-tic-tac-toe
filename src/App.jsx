5319ba55390cf1e8141e2cfcba1f1583b59c61a5
Zach Barge <zachb.barge@gmail.com>
2023-12-08 12:37:17 -0500

    Got history saving implemented properly.

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,21 @@ function Square({ value, onSquareClick }) {
 	return <button onClick={onSquareClick} className="square">{value}</button>
 }
 
-function Board() {
-	const [squares, setSquares] = useState(Array(9).fill(null))
-	const [isXTurn, setIsXTurn] = useState(true)
+function Board({ xIsNext, squares, onPlay }) {
 
 	function handleClick(index) {
 		if (squares[index] || calculateWinner(squares)) return
 
 		const newSquares = squares.slice()
 
-		if (isXTurn) {
+		if (xIsNext) {
 			newSquares[index] = 'X'
 		} else {
 			newSquares[index] = 'O'
 		}
 
-		setIsXTurn(!isXTurn)
-		setSquares(newSquares)
+		onPlay(newSquares);
+
 	}
 
 	const winner = calculateWinner(squares)
@@ -31,9 +29,9 @@ function Board() {
 	let status;
 
 	if (winner) {
-		status = 'Winner' + winner
+		status = 'Winner: ' + winner
 	} else {
-		status = 'Next player: ' + (isXTurn ? 'X' : 'O')
+		status = 'Next player: ' + (xIsNext ? 'X' : 'O')
 	}
 
 	return (<>
@@ -59,12 +57,20 @@ function Board() {
 }
 
 function Game() {
+	const [xIsNext, setXIsNext] = useState(true)
+	const [history, setHistory] = useState([Array(9).fill(null)])
 
+	const currentSquares = history[history.length - 1]
+
+	function handlePlay(newSquares) {
+		setXIsNext(!xIsNext)
+		setHistory([...history, newSquares])
+	}
 
 	return (
 		<div className="game">
 			<div className="game-board">
-				<Board />
+				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
 			</div>
 			<div className="game-info">
 				<ol></ol>
@@ -74,23 +80,23 @@ function Game() {
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 export default Game
